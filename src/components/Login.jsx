3d69b9695fcd344d504c07984c4ac329b944778e
priyanshimaheshwari <priyanshimaheshwari16@gmail.com>
2022-12-23 23:12:30 +0530

    new

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,10 +13,10 @@ const Login = () => {
         </div>
         <div className='flex justify-center items-center h-full'>
            <form className='max-w-[400px] w-full mx-auto bg-white p-2 '>
-               <h2 className='text-4xl font-bold text-center py-3'>Code <span className='text-gray-500'>Hack</span></h2>
+               <h2 className='text-4xl font-bold text-center py-3 '>Code <span className='text-gray-500'>Hack</span></h2>
                <div className='flex justify-around  py-2'>
-               <p className='border shadow-lg hover:shadow-xl px-4 py-2 relative flex items-center rounded-2xl'><AiFillFacebook className='mr-2' /><a href='https://www.facebook.com/login/'><button>Facebook</button></a></p>
-                   <p className='border shadow-lg hover:shadow-xl px-4 py-2 relative flex items-center rounded-2xl '><FcGoogle className='mr-4' /><a href="https://www.google.com/"><button>Google</button></a></p>
+               <p className='border shadow-lg hover:shadow-xl px-4 py-2 relative flex items-center rounded-2xl hover:bg-indigo-300'><AiFillFacebook className='mr-2' /><a href='https://www.facebook.com/login/'><button>Facebook</button></a></p>
+                   <p className='border shadow-lg hover:shadow-xl px-4 py-2 relative flex items-center rounded-2xl hover:bg-indigo-300'><FcGoogle className='mr-4' /><a href="https://www.google.com/"><button>Google</button></a></p>
                    </div>
                    <div className='flex flex-col mb-4'>
                        <label>Username</label>
@@ -24,7 +24,7 @@ const Login = () => {
                        <label>Password</label>
                        <input className='border relative bg-gray-100 p-1 mr-2 rounded-2xl'type='password' />
                    </div>
-                   <button className='w-full py-3 mt-2 bg-gray-600 hover:bg-indigo-500 relative text-white rounded-2xl'>Sign In</button>
+                   <button className='w-full py-3 mt-2 bg-gray-600 hover:bg-indigo-300 relative text-white rounded-2xl'>Sign In</button>
                    <p className='flex items-center mt-2'><input className='mr-2 relative' type="checkbox"  />Remember Me</p>
                    Not a member? 
             <button  className='text-center mt-2 relative text-gray-300 hover:text-indigo-400'> Sign Up</button>
